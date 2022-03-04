a7dc1db42bc91816fc9a047724d6770354e2610c
Alan Magano <alan.magano@kukac.com.br>
2022-03-04 10:36:33 -0300

    Commit inicial

---
+++ teste.py
@@ -0,0 +1,12 @@
+# Este teste eu fiz sozinho
+
+from models.produto import Produto
+from time import sleep
+
+poly = Produto('PolyStation', 150.99)
+ps2 = Produto('PlayStation 2', 600.59)
+
+print(poly)
+print(ps2)
+print(f'Fechando... ')
+sleep(5)
